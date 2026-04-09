eb3de6ddb47b106b5d9da4d3b2ae0dce6684d6bb
Aaron <toscleveland@gmail.com>
2026-04-09 01:35:27 -0500

    Add Cask for Dopple version 1.0.0

---
+++ Casks/dopple.rb
@@ -0,0 +1,20 @@
+cask "dopple" do
+  version "1.0.0"
+  sha256 "YOUR_SHA256_HERE"
+
+  url "https://pub-6f2292da664442f4abb42bde414c777b.r2.dev/dopple-#{version}.dmg"
+  name "Dopple"
+  desc "Git profile switcher for developers"
+  homepage "https://doppleapp.com"
+
+  depends_on macos: ">= :sequoia"
+
+  app "Dopple.app"
+
+  zap trash: [
+    "~/.git-switcher",
+    "~/Library/Preferences/com.dopple.app.plist",
+    "~/Library/Application Support/Dopple",
+    "~/Library/Caches/Dopple",
+  ]
+end
