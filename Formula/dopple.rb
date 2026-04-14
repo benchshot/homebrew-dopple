38091ddd5f415dde546ad8b4cdcf4b904d4ed321
Aaron Cleveland <toscleveland@gmail.com>
2026-04-13 19:17:39 -0500

    dopple 0.0.4 (use R2 URL)

--- Formula/dopple.rb
+++ Formula/dopple.rb
@@ -1,9 +1,9 @@
 class Dopple < Formula
   desc "AI terminal assistant for developers"
   homepage "https://doppleapp.com"
-  url "https://github.com/benchshot/Dopple/releases/download/v0.0.3/dopple"
+  url "https://releases.doppleapp.com/dopple-v0.0.4"
   sha256 "00b3594458552cc1d918f3ef3bcd8604abc4268379c48c860eafd5d13b98e931"
-  version "0.0.3"
+  version "0.0.4"
 
   def install
     bin.install "dopple"
